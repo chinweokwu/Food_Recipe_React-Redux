e1c04cc95244164ac96b24db83c905a3738ec871
chinweokwu <paulcmorah@gmail.com>
2021-03-12 16:52:05 +0100

    add action

---
+++ src/Action/index.js
@@ -0,0 +1,19 @@
+import {
+  FETCH_USER_REQUEST,
+  FETCH_USER_SUCCESS,
+  FETCH_USER_FAILURE,
+} from './actionTypes';
+
+export const fetchUserRequest = () => ({
+  type: FETCH_USER_REQUEST,
+});
+
+export const fetchUserSuccess = data => ({
+  type: FETCH_USER_SUCCESS,
+  payload: data,
+});
+
+export const fetchUserFailure = error => ({
+  type: FETCH_USER_FAILURE,
+  payload: error,
+});
