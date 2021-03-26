b4c4488e10f43d910ccc3bbc23aee93361eadf10
chinweokwu <paulcmorah@gmail.com>
2021-03-26 14:43:57 +0100

    add styles to recipe page

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -3,6 +3,7 @@
 // /* eslint-disable react/prop-types */
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Recipe = ({ location }) => {
   const [activeMenu, setActiveMenu] = useState([]);
@@ -12,22 +13,48 @@ const Recipe = ({ location }) => {
     const title = location.state.food;
     axios.get(`https://serene-gorge-49314.herokuapp.com/https://recipesapi.herokuapp.com/api/search?q=${title}`)
       .then(response => {
-        console.log(response.data.recipes[0]);
         setActiveMenu(response.data.recipes[0]);
       });
   }, []);
-
+  const backgroundUrl = activeMenu.image_url;
   return (
-    <div>
-      <div className="card">
-        <div>
-          <img src={activeMenu.image_url} alt={activeMenu.title} />
+
+    <div
+      style={{
+        position: 'fixed',
+        left: '0',
+        right: '0',
+        zIndex: '-1',
+        display: 'block',
+        backgroundImage: `url(${backgroundUrl})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <div className="wrapper">
+        <div
+          style={{
+            backgroundImage: `url(${backgroundUrl})`,
+            imageRendering: 'auto',
+            width: '500px',
+            height: '300px',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            margin: 'auto',
+          }}
+        />
+        <div className="wrapper-title">
+          <p>{ activeMenu.title }</p>
+          <p>{ activeMenu.publisher }</p>
+          <p>{ activeMenu.publisher_url }</p>
+          <p>{ activeMenu.recipe_id }</p>
+          <p>{ activeMenu.source_url }</p>
+          <button type="submit" className="wrapper-buttton">
+            <Link to="/"> Go Home</Link>
+          </button>
         </div>
-        { activeMenu.title }
-        { activeMenu.publisher }
-        { activeMenu.publisher_url }
-        { activeMenu.recipe_id }
-        { activeMenu.source_url }
       </div>
     </div>
   );
