2d86d83d7d82bf31d20e1ca091909e79f00d2688
chinweokwu <paulcmorah@gmail.com>
2021-03-26 16:51:18 +0100

    add proptypes

--- src/Containers/menu.js
+++ src/Containers/menu.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { fetchRecipes, searchRecipes } from '../Action/fetchActions';
 
 class Menu extends Component {
@@ -19,7 +17,7 @@ class Menu extends Component {
 
   render() {
     const searchRecipes = this.props.recipeData.map(recipe => (
-      <div className="card" key={recipe._id}>
+      <div className="card" key={recipe.recipe_id}>
         <div className="card_image">
           <img src={recipe.image_url} alt={recipe.title} />
         </div>
@@ -51,17 +49,20 @@ class Menu extends Component {
           <button type="submit" className="search-btn">Search</button>
         </form>
         <div className="main">
-          {this.props.recipeData.loading ? (
-            <h2 className="loading">Loading...</h2>
-          ) : this.props.recipeData.error ? (
-            <h2>{this.props.error}</h2>
-          ) : (searchRecipes)}
+          { searchRecipes }
         </div>
       </div>
     );
   }
 }
 
+Menu.propTypes = {
+  searchRecipes: PropTypes.func.isRequired,
+  fetchRecipes: PropTypes.func.isRequired,
+  recipeData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  text: PropTypes.string.isRequired,
+
+};
 const mapStateToProps = state => ({
   recipeData: state.results.recipes,
   text: state.results.text,
